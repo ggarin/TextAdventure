70a9f154b5a04414e61db9d051ef026f920cb729
ThT12 <ThT12@ThT12.fr>
2017-04-30 18:26:00 +0200

    add real story

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from world import World
 
 
 print('Welcome to Text Adventure Game - v0.1')
-
-my_world = World('ThT12')
-my_world.display()
+name = input('What is your name?')
+print('Hi ' + name + ' A little girl have disappear! Can you find him in the manor ?')
+input('Press enter to start your quest!')
+my_world = World(name)
 my_world.run_game()
--- world.py
+++ world.py
@@ -1,19 +1,50 @@
+import numpy
 from room import Room
 from hero import Hero
-from directions import move
-from directions import Direction
-from directions import Directions
-import numpy
+from directions import Direction, Directions, move
 
 
 class World:
     def __init__(self, name=None):
-        room1 = Room('Room1', 'First room', Directions([Direction.NORTH, Direction.EAST]))
-        room2 = Room('Room2', 'Second room', Directions([Direction.NORTH, Direction.WEST]))
-        room3 = Room('Room3', 'Third room', Directions([Direction.SOUTH, Direction.EAST]))
-        room4 = Room('Room4', 'Fourth room', Directions([Direction.SOUTH, Direction.WEST]), True)
-        self.room_table = numpy.matrix([[room1, room2], [room3, room4]], dtype=Room)
-        self.hero = Hero(name, self.room_table[0, 0])
+        room3 = Room('Start', 'You just enter into a manor ! You are in a big room with a nice carpet. The door '
+                              'behind you close itself. You now have to choose in three direction.',
+                     Directions([Direction.WEST, Direction.NORTH, Direction.EAST]))
+        room2 = Room('Kitchen', 'You enter into a kitchen. Some meal are under preparation but no one is here. You see '
+                                'a door in front of you or you can just go back.',
+                     Directions([Direction.WEST, Direction.EAST]))
+        room1 = Room('Closet', 'You found a closet with a lot of cooking stuff. You have to go back',
+                     Directions([Direction.EAST]))
+        room4 = Room('Domestic Chamber', 'You enter into a small chamber with a simple bed and a piece of furniture and'
+                                         ' a small window. There is a book on a chair close to the bed.',
+                     Directions([Direction.WEST]))
+        room7 = Room('Living room', 'This is the first part of the living room. There is shelf on the wall  with a lot '
+                                    'of books. You can explore the living room in two directions',
+                     Directions([Direction.NORTH, Direction.EAST, Direction.SOUTH]))
+        room8 = Room('Living room', 'This is another part of the living room. There is a big table with some plates and'
+                                    ' cutlery ready for eating.',
+                     Directions([Direction.WEST, Direction.NORTH]))
+        room12 = Room('Living room', 'This is another part of the living room. There is a sofa and a coffee table. You '
+                                     'can see three half empty glasses. You see a close chest under the windows.',
+                      Directions([Direction.WEST, Direction.NORTH, Direction.SOUTH]))
+        room16 = Room('Chest', 'You open the chest and found bottles of alcohol.',
+                      Directions([Direction.SOUTH]))
+        room11 = Room('Living room', 'This is another part of the living room. You can see a stair going down in North '
+                                     'and a long hallway in the West.',
+                      Directions([Direction.WEST, Direction.NORTH, Direction.EAST, Direction.SOUTH]))
+        room15 = Room('Stairs', 'Some dark stairs are going down to a toilet.',
+                      Directions([Direction.SOUTH]))
+        room10 = Room('Hallway', 'A simple hallway with chandelier give access to two rooms.',
+                      Directions([Direction.WEST, Direction.NORTH, Direction.EAST]))
+        room14 = Room('Children Chamber', 'An empty children chamber with a lot of toys on the floor.',
+                      Directions([Direction.SOUTH]))
+        room9 = Room('Adults Chamber', 'A big room with a double bed. You can see a light passing though a door in '
+                                       'the south.',
+                     Directions([Direction.EAST, Direction.SOUTH]))
+        room5 = Room('Bathroom', 'You open the door and find a bathroom. You discover the girl afraid and crying !',
+                     Directions([Direction.EAST, Direction.SOUTH]), True)
+        self.room_table = numpy.matrix([[room1, room2, room3, room4], [room5, Room(), room7, room8],
+                                        [room9, room10, room11, room12], [Room(), room14, room15, room16]], dtype=Room)
+        self.hero = Hero(name, self.room_table[0, 2])
 
     def display(self):
         room_shape = self.room_table.shape
