f94de8e0dca99114b405d57c28c14675dd2a70cc
ThT12 <ThT12@ThT12.fr>
2017-04-30 12:14:44 +0200

    first object

---
+++ main.py
@@ -0,0 +1,5 @@
+from room import Room
+print('Welcome to Text Adventure Game - v0.1')
+
+myRoom = Room()
+myRoom.display()
---
+++ room.py
@@ -0,0 +1,16 @@
+class Room:
+    # define a room
+    def __init__(self):
+        self.name = 'DefaultRoom'
+
+    def display(self):
+        print(self.name)
+
+
+def main():
+    my_room = Room()
+    my_room.display()
+
+
+if __name__ == "__main__":
+    main()
