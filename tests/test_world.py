79b3326208c579df2a3bcf7698efda853a771780
ThT12 <ThT12@ThT12.fr>
2017-05-07 12:04:15 +0200

    add test for run_game --> coverage 100%

--- tests/test_world.py
+++ tests/test_world.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from textadventure.world import World
 from textadventure.directions import Direction
@@ -23,3 +24,18 @@ class TestWorld(TestCase):
     def test_get_room_after_move_W(self):
         new_room = self.my_world.get_room_after_move(direction=Direction.WEST)
         self.assertEqual(new_room, self.my_world.room_table[1, 1])
+
+
+class TestGame(TestCase):
+    def setUp(self):
+        self.my_world = World()
+
+    @patch('builtins.input')
+    def test_win(self, mock_input):
+        mock_input.side_effect = ['W', 'W', 'E', 'E', 'E', 1, 'W', 'N', 'N', 'E', 'N', 'S', 'W', 'W', 2, 'W', 'S', 3]
+        self.my_world.run_game()
+
+    @patch('builtins.input')
+    def test_loose(self, mock_input):
+        mock_input.side_effect = ['N', 'N', 'W', 1]
+        self.my_world.run_game()
